7df22734e1f067769e7a411f209a3ee82d9591f7
Lukas Arnold <lukas.arnold@bristol.ac.uk>
2017-02-15 14:09:13 -0500

    readme and gitadd updated

--- gitadd.sh
+++ gitadd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-git add *.py modules/*.py scripts/*.py utils/*.py libs/*.csv libs/*.py
+git add *.py modules/*.py scripts/*.py utils/*.py libs/*.csv libs/*.py *.md requirements/* *.sh
